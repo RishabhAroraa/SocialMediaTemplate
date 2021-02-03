e3751a8fb583d4b3a92fe29484ea0df3995dff0f
RishabhAroraa <rishabharora.ns@gmail.com>
2021-02-03 20:15:08 +0530

    updated auth.py

--- website/auth.py
+++ website/auth.py
@@ -4,4 +4,8 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('login')
 def login():
-	return "<h1>This is the login page</h1>"
+	return "<h1>Logged in</h1>"
+
+@auth.route('logout')
+def logout():
+	return "<h1>Logged out</h1>"
