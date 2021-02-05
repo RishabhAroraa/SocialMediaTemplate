65b91ce4dc5d5caa688f694436629550b12c2ddd
RishabhAroraa <rishabharora.ns@gmail.com>
2021-02-05 15:42:52 +0530

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -6,15 +6,25 @@ auth = Blueprint('auth', __name__)
 def signup():
 	if request.method == 'POST':
 		data = request.form
-		print(data)
+		uname = data.get('uname')
+		email = data.get('email')
+		passwd = data.get('passwd')
+		passwd2 = data.get('passwd2')
+		print('New signup-> \nName: {}\nPassword: {}\nEmail: {}'.format(uname, passwd, email))
+
 	return render_template('signup.html')
+
 
 @auth.route('login', methods=['GET', 'POST'])
 def login():
 	if request.method == 'POST':
 		data = request.form
-		print(data)
+		uname = data.get('uname')
+		passwd = data.get('passwd')
+		print('New login-> \nName: {}\nPassword: {}\n'.format(uname,passwd))
+
 	return render_template('login.html')
+
 
 @auth.route('logout')
 def logout():
