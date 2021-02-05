1fc55985f9e1635163c2b07776398a3cd4b563ee
RishabhAroraa <rishabharora.ns@gmail.com>
2021-02-05 15:13:52 +0530

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('signup')
+def signup():
+	return render_template('signup.html')
+
 @auth.route('login')
 def login():
 	return render_template('login.html')
