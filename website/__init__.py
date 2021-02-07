55219f76df1b87cfcf18516a9ff3ad07b60af96a
Rishabh Arora <43946451+RishabhAroraa@users.noreply.github.com>
2021-02-07 15:28:27 +0530

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
 	app = Flask(__name__)
-	app.config['SECRET_KEY'] = '$3cr3t-k3Y'
+	app.config['SECRET_KEY'] = '8D49F882EC744223E64E7B143C6E4'
 
 	from .views import views
 	app.register_blueprint(views, url_prefix='/')
